ddb4448a6001dc8626a31c7399bd8213e8078601
Connell Boyce <connell.p.boyce@gmail.com>
2021-02-06 02:06:35 -0500

    Adding initial Spring files
    - Only dependencies are Spring Web and Thymeleaf
    - Java 8
    - Spring Boot 2.4.2
    - Maven
    - Jar Packaging

---
+++ src/test/java/com/cmplxsoftsys/team3/financeapplication/FinanceapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmplxsoftsys.team3.financeapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
