e5ea88422a2d07efc4f9f55828e61d4a4a604598
Cole <coleipod@gmail.com>
2021-02-16 10:56:04 -0500

    Added basic Spring JUnit tests for PoC

--- src/main/java/com/cmplxsoftsys/team3/financeapplication/controller/WebController.java
+++ src/main/java/com/cmplxsoftsys/team3/financeapplication/controller/WebController.java
@@ -1,7 +1,9 @@
 package com.cmplxsoftsys.team3.financeapplication.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class WebController {
 
     @GetMapping("/")
