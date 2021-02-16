6e55eee1d444102d03c5d8d4ed3080cad05cdc79
averagejoe127 <tdb32@psu.edu>
2021-02-16 10:38:48 -0500

    Added basic controller and tested git functionality

--- src/main/java/com/cmplxsoftsys/team3/financeapplication/controller/WebController.java
+++ src/main/java/com/cmplxsoftsys/team3/financeapplication/controller/WebController.java
@@ -1,4 +1,11 @@
 package com.cmplxsoftsys.team3.financeapplication.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class WebController {
+
+    @GetMapping("/")
+    public String getHomepage() {
+        return "index";
+    }
 }
