9687558aecc004bdf94614e051bd2fee179f37b5
averagejoe127 <49156743+averagejoe127@users.noreply.github.com>
2021-02-21 17:30:11 -0500

    Update User.java

--- src/main/java/com/cmplxsoftsys/team3/financeapplication/model/User.java
+++ src/main/java/com/cmplxsoftsys/team3/financeapplication/model/User.java
@@ -3,9 +3,9 @@ package com.cmplxsoftsys.team3.financeapplication.model;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+//import javax.validation.constraints.Email;
+//import javax.validation.constraints.NotBlank;
+//import javax.validation.constraints.Size;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
