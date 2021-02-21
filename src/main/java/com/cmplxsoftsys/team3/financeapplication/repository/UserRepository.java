60bb3b134c68da83ae4c683176333c45ccd6f5fd
Cole <coleipod@gmail.com>
2021-02-21 18:02:21 -0500

    Added JavaDoc

--- src/main/java/com/cmplxsoftsys/team3/financeapplication/repository/UserRepository.java
+++ src/main/java/com/cmplxsoftsys/team3/financeapplication/repository/UserRepository.java
@@ -4,8 +4,29 @@ import com.cmplxsoftsys.team3.financeapplication.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
+/**
+ * Interface to allow querying users based on the backend Mongo database.
+ */
 public interface UserRepository extends MongoRepository<User, String> {
+    /**
+     * Searches for a user in the DB and returns if found.
+     * @param username username to search for
+     * @return User object if found, otherwise nothing.
+     */
     Optional<User> findByUsername(String username);
+
+    /**
+     * Searches for a user in the DB and says if the username was found.
+     * @param username username to search for
+     * @return if username is in use already
+     */
     Boolean existsByUsername(String username);
+    
+    
+    /**
+     * Searches for a user in the DB and says if the email was found.
+     * @param email email address to search for
+     * @return if email is in use already
+     */
     Boolean existsByEmail(String email);
 }
