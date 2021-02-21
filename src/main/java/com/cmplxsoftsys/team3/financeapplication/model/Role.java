55fa46bb4a5d3e07c230875a56d3704e7db52f39
averagejoe127 <49156743+averagejoe127@users.noreply.github.com>
2021-02-21 16:44:53 -0500

    Update Role.java

--- src/main/java/com/cmplxsoftsys/team3/financeapplication/model/Role.java
+++ src/main/java/com/cmplxsoftsys/team3/financeapplication/model/Role.java
@@ -11,22 +11,27 @@ public class Role {
   public Role() {
   }
 
+  //Constructor
   public Role(ERole name) {
     this.name = name;
   }
 
+  //Returns userID
   public String getId() {
     return id;
   }
 
+  //Sets ID of user to specified ID
   public void setId(String id) {
     this.id = id;
   }
 
+  //Returns userName
   public ERole getName() {
     return name;
   }
 
+  //Sets userName
   public void setName(ERole name) {
     this.name = name;
   }
