d8cb60f838e6b8f5a476163375297fa5ffd1d144
averagejoe127 <49156743+averagejoe127@users.noreply.github.com>
2021-02-21 16:47:38 -0500

    Update User.java

--- src/main/java/com/cmplxsoftsys/team3/financeapplication/model/User.java
+++ src/main/java/com/cmplxsoftsys/team3/financeapplication/model/User.java
@@ -25,48 +25,59 @@ public class User {
   public User() {
   }
 
+  //Constructor
   public User(String username, String email, String password) {
     this.username = username;
     this.email = email;
     this.password = password;
   }
 
+  //Returns UserID
   public String getId() {
     return id;
   }
 
+  //Sets userID
   public void setId(String id) {
     this.id = id;
   }
 
+  //Returns userName
   public String getUsername() {
     return username;
   }
 
+  //Sets userName
   public void setUsername(String username) {
     this.username = username;
   }
 
+  //Returns user Email
   public String getEmail() {
     return email;
   }
 
+  //Sets user Email
   public void setEmail(String email) {
     this.email = email;
   }
 
+  //Returns user password
   public String getPassword() {
     return password;
   }
 
+  //Sets user password
   public void setPassword(String password) {
     this.password = password;
   }
 
+  //Returns the roles of the user
   public Set<Role> getRoles() {
     return roles;
   }
 
+  //Sets roles for a user
   public void setRoles(Set<Role> roles) {
     this.roles = roles;
   }
