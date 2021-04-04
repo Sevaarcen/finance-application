967e9b1b6e48e8a058ffcc6003462e4635d2962a
averagejoe127 <tdb32@psu.edu>
2021-04-04 17:16:16 -0400

    Ajax call added to post loan application to database

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -7,3 +7,31 @@ loanRangeSliderOutput.innerText = loanRangeSlider.value;
 loanRangeSlider.oninput = function () {
     loanRangeSliderOutput.innerHTML = '$' + this.value;
 }
+
+function recordNewLoan() {
+
+    var newLoan = {
+        'type': document.getElementByID('icons-sm'),
+        'value': loanRangeSliderOutput,
+        'userID': localStorage.getItem('id')
+    };
+    //API call to post this new loan application to the loan application database
+    $.ajax({
+        url: '/api/loan/request',
+        type: "POST",
+        dataType: 'json',
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(newLoan),
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('Authorization', localStorage.getItem('AuthorizationHeader'));
+        },
+        success: function (result) {
+            console.log("Successfully submitted!");
+            location.reload();
+        },
+        error: function (xhr, resp, text) {
+            console.log(xhr, resp, text);
+        }
+    })
+
+}
